f3a4f1d20afdd9bf0b198161aee79f6a7609a21d
foysalious <foysalrahman112@gmail.com>
2020-09-08 12:50:28 +0600

    updated

--- FinalExam/database/migrations/2014_10_12_000000_create_users_table.php
+++ FinalExam/database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,10 +14,11 @@ class CreateUsersTable extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->id();
+            $table->bigIncrements('id');
             $table->string('name');
-            $table->string('email')->unique();
+            $table->string('email');
             $table->timestamp('email_verified_at')->nullable();
+            $table->boolean('is_admin')->nullable();
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
--- FinalExam/routes/web.php
+++ FinalExam/routes/web.php
@@ -24,3 +24,4 @@ Route::get('/home', 'HomeController@index')->name('home');
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+Route::get('admin/home', 'HomeController@adminHome')->name('admin.home')->middleware('is_admin');
